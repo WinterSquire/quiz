babda63c0d801151d27b7b2439194de5813c370f
WinterSquire <135317392+WinterSquire@users.noreply.github.com>
2025-07-26 11:16:00 +0800

    update libmcc
    add comments to problems

--- src/problem/p1.cpp
+++ src/problem/p1.cpp
@@ -1,5 +1,9 @@
 #include "problem.h"
 
+/*
+	Problem 1: Camera Properties
+	- Display and modify camera properties.
+*/
 int problem1(bool* show, const s_game_context* game, s_camera_context* camera) {
     int status = 0;
     do {
--- src/problem/p2.cpp
+++ src/problem/p2.cpp
@@ -1,5 +1,10 @@
 #include "problem.h"
 
+/*
+	Problem 2: Player Profile Settings
+	- Display and modify player profile settings.
+	- Player can choose to save or discard changes.
+*/
 int problem2(bool* show, const s_game_context* game, s_profile_context* profile) {
     int status = 0;
     do {
@@ -7,6 +12,8 @@ int problem2(bool* show, const s_game_context* game, s_profile_context* profile)
             break;
         // write code here
         // do not use return, use /* status = -1; break; */ instead.
+
+        auto _profile = profile->profile->get();
 
     } while (false);
     ImGui::End();
